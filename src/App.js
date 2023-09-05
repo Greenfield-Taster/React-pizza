6adbae51d87bb1712756f8224b4b7b2f97580fa6
Asya Gorbatchova <gorbatchovaasa123@gmail.com>
2023-09-05 12:57:21 +0300

    Add useState to button addPizza

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import "./scss/app.scss";
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
             <PizzaBlock title="Something1" price={500} />
             <PizzaBlock title="Something2" price={300} />
             <PizzaBlock title="Something3" price={600} />
