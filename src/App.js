3f12de7c16d84225a2c11ad55b3f69a63ca695aa
Asya Gorbatchova <gorbatchovaasa123@gmail.com>
2023-11-21 18:40:00 +0200

    Add a counter using Redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/filterSlice";
+
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
@@ -9,16 +13,34 @@ import Cart from "./pages/Cart";
 import "./scss/app.scss";
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div className="wrapper">
-      <Header />
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+
+      {/* <Header />
       <div className="content">
         <Routes>
           <Route path="React-pizza/" element={<Home />} />;
           <Route path="/cart" element={<Cart />} />;
           <Route path="*" element={<NotFound />} />;
         </Routes>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import { Provider } from "react-redux";
+
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
