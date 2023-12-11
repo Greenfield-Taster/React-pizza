9eebbae35aa80882e099c9cc45e795a3f8737793
Anastasiia Horbachova <gorbatchovaasa123@gmail.com>
2023-12-11 06:03:19 +0200

    Add object of all pizzas and user info to the server; add dispatch(clearItems) and moving to the another route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
--- src/components/CartOrder.jsx
+++ src/components/CartOrder.jsx
@@ -1,37 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useForm, Controller } from "react-hook-form";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import axios from "axios";
-// import { getCartFromLS } from "../utils/getCartFromLS";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import CartOrderDone from "./CartOrderDone";
 
 import privatterminal from "../assets/img/privatterminal_color.png";
 import googlePay from "../assets/img/googlepay_color.svg";
-
-// const { totalPrice } = getCartFromLS();
+import { clearItems } from "../redux/slices/cartSlice";
 
 const CartOrder = () => {
-  const { totalPrice } = useSelector((state) => state.cart);
   const { register, handleSubmit, formState, control } = useForm();
   const { errors } = formState;
 
+  const dispatch = useDispatch();
+  const { totalPrice } = useSelector((state) => state.cart);
+
   const [selectedPayment, setSelectedPayment] = React.useState("");
   const [cartData, setCartData] = React.useState([]);
+  const [formSubmitted, setFormSubmitted] = React.useState(false);
 
   const handlePaymentSelection = (paymentMethod) => {
     setSelectedPayment(paymentMethod);
   };
 
+  const finishSubmitting = () => {
+    setFormSubmitted(true);
+    dispatch(clearItems());
+  };
+
   const onSubmit = async (data) => {
     data.paymentMethod = selectedPayment;
+    data.dataOfOrderedPizzas = cartData;
     console.log("Form submitted", data);
 
     await axios
       .post("https://pizzas-backend.azurewebsites.net/users", data)
       .then((response) => {
         console.log("Server response:", response.data);
+        finishSubmitting();
       })
       .catch((error) => {
         console.error("Error submitting form:", error.response.data.message);
@@ -47,175 +55,186 @@ const CartOrder = () => {
 
   return (
     <div className="cart cart--order">
-      <>
-        <div className="pizza-order-container">
-          <h1>Your order</h1>
-          <div className="pizza-order-items">
-            {cartData.map((item, index) => (
-              <div className="pizza-order" key={item._id}>
-                <div className="pizza-info">
-                  <div className="selected-description-full">
-                    <h3>{item.title}</h3>
-                    <div className="selected-description">
-                      <p>{item.size} sm</p>,<p>{item.type}</p>
+      {formSubmitted ? (
+        <CartOrderDone />
+      ) : (
+        <>
+          <div className="user-order">
+            <div className="pizza-order-container">
+              <h1>Your order</h1>
+              <div className="pizza-order-items">
+                {cartData.map((item) => (
+                  <div className="pizza-order" key={item._id}>
+                    <div className="pizza-info">
+                      <div className="selected-description-full">
+                        <h3>{item.title}</h3>
+                        <div className="selected-description">
+                          <p>{item.size} sm</p>,<p>{item.type}</p>
+                        </div>
+                      </div>
+                      <h4>{item.count}</h4>
+                      <h4>$ {item.price}</h4>
                     </div>
                   </div>
-                  <h4>{item.count}</h4>
-                  <h4>$ {item.price}</h4>
+                ))}
+                <div className="total-order-price">
+                  <h4>TOTAL DUE: </h4>
+                  <h2>{totalPrice}$</h2>
                 </div>
               </div>
-            ))}
-            <div className="total-order-price">
-              <h4>TOTAL DUE: </h4>
-              <h2>{totalPrice}$</h2>
             </div>
           </div>
-        </div>
-      </>
-      <>
-        <h1>Checkout</h1>
-        <form action="quickOrder" onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            {...register("name", { required: "Name is required" })}
-          />
-          {errors.name && <p className="errorText">{errors.name.message}</p>}
-          <label htmlFor="surname">Surname</label>
-          <input
-            type="text"
-            id="surname"
-            {...register("surname", { required: "Surname is required" })}
-          />
-          {errors.surname && (
-            <p className="errorText">{errors.surname.message}</p>
-          )}
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="user@gmail.com"
-            {...register(
-              "email",
-              {
-                pattern: {
-                  value:
-                    /^[a-zA-Z0-9.!#$%&'*+/=?^{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/,
-                  message: "Invalid email format",
-                },
-              },
-              { required: "Email is required" }
-            )}
-          />
-          {errors.email && <p className="errorText">{errors.email.message}</p>}
-          <label htmlFor="userPhoneNumber">Phone</label>
-          <div className="phoneForm">
-            <Controller
-              control={control}
-              name="phone"
-              placeholder="1 (702) 123-4567"
-              rules={{ required: true }}
-              render={({ field: { ref, ...field } }) => (
-                <PhoneInput
-                  {...field}
-                  inputExtraProps={{
-                    ref,
-                    required: true,
-                    autoFocus: true,
-                  }}
-                  country={"ua"}
-                  // onlyCountries={["ua"]}
-                  countryCodeEditable={false}
-                />
+
+          <div className="checkout-order">
+            <h1>Checkout</h1>
+            <form action="quickOrder" onSubmit={handleSubmit(onSubmit)}>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                {...register("name", { required: "Name is required" })}
+              />
+              {errors.name && (
+                <p className="errorText">{errors.name.message}</p>
               )}
-            />
-          </div>
-          <div className="form-group">
-            <h2>Delivery Type</h2>
-            <div className="radio-options">
-              <div>
-                <input
-                  type="radio"
-                  id="delivery"
-                  {...register("deliveryType", {
-                    required: "Please select a delivery option",
-                  })}
-                  value="delivery"
+              <label htmlFor="surname">Surname</label>
+              <input
+                type="text"
+                id="surname"
+                {...register("surname", { required: "Surname is required" })}
+              />
+              {errors.surname && (
+                <p className="errorText">{errors.surname.message}</p>
+              )}
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="user@gmail.com"
+                {...register(
+                  "email",
+                  {
+                    pattern: {
+                      value:
+                        /^[a-zA-Z0-9.!#$%&'*+/=?^{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/,
+                      message: "Invalid email format",
+                    },
+                  },
+                  { required: "Email is required" }
+                )}
+              />
+              {errors.email && (
+                <p className="errorText">{errors.email.message}</p>
+              )}
+              <label htmlFor="userPhoneNumber">Phone</label>
+              <div className="phoneForm">
+                <Controller
+                  control={control}
+                  name="phone"
+                  placeholder="1 (702) 123-4567"
+                  rules={{ required: true }}
+                  render={({ field: { ref, ...field } }) => (
+                    <PhoneInput
+                      {...field}
+                      inputExtraProps={{
+                        ref,
+                        required: true,
+                        autoFocus: true,
+                      }}
+                      country={"ua"}
+                      // onlyCountries={["ua"]}
+                      countryCodeEditable={false}
+                    />
+                  )}
                 />
-                <label htmlFor="delivery">🚚 Delivery</label>
               </div>
-              <div>
-                <input
-                  type="radio"
-                  id="pickup"
-                  {...register("deliveryType", {
-                    required: "Please select a delivery option",
-                  })}
-                  value="pickup"
-                />
-                <label htmlFor="pickup">🏠 Self-Pickup</label>
-              </div>
-            </div>
-            {errors.deliveryType && (
-              <p className="errorText">{errors.deliveryType.message}</p>
-            )}
-          </div>
-          <div className="pay-form">
-            <div className="pay-form-select">
-              <div>
-                <h3>CASH</h3>
-                <div
-                  className={`payment-option ${
-                    selectedPayment === "cash" ? "selected" : ""
-                  }`}
-                  onClick={() => handlePaymentSelection("cash")}
-                >
-                  <img src={privatterminal} alt="Cash Payment" />
-                  <input
-                    type="radio"
-                    id="cash"
-                    value="cash"
-                    {...register("paymentMethod", {
-                      required: "Please select a payment method",
-                    })}
-                    onChange={() => handlePaymentSelection("cash")}
-                    checked={selectedPayment === "cash"}
-                  />
+              <div className="form-group">
+                <h2>Delivery Type</h2>
+                <div className="radio-options">
+                  <div>
+                    <input
+                      type="radio"
+                      id="delivery"
+                      {...register("deliveryType", {
+                        required: "Please select a delivery option",
+                      })}
+                      value="delivery"
+                    />
+                    <label htmlFor="delivery">🚚 Delivery</label>
+                  </div>
+                  <div>
+                    <input
+                      type="radio"
+                      id="pickup"
+                      {...register("deliveryType", {
+                        required: "Please select a delivery option",
+                      })}
+                      value="pickup"
+                    />
+                    <label htmlFor="pickup">🏠 Self-Pickup</label>
+                  </div>
                 </div>
+                {errors.deliveryType && (
+                  <p className="errorText">{errors.deliveryType.message}</p>
+                )}
               </div>
+              <div className="pay-form">
+                <div className="pay-form-select">
+                  <div>
+                    <h3>CASH</h3>
+                    <div
+                      className={`payment-option ${
+                        selectedPayment === "cash" ? "selected" : ""
+                      }`}
+                      onClick={() => handlePaymentSelection("cash")}
+                    >
+                      <img src={privatterminal} alt="Cash Payment" />
+                      <input
+                        type="radio"
+                        id="cash"
+                        value="cash"
+                        {...register("paymentMethod", {
+                          required: "Please select a payment method",
+                        })}
+                        onChange={() => handlePaymentSelection("cash")}
+                        checked={selectedPayment === "cash"}
+                      />
+                    </div>
+                  </div>
 
-              <div>
-                <h3>CASHLESS</h3>
-                <div
-                  className={`payment-option ${
-                    selectedPayment === "cashless" ? "selected" : ""
-                  }`}
-                  onClick={() => handlePaymentSelection("cashless")}
-                >
-                  <img src={googlePay} alt="Cashless Payment" />
-                  <input
-                    type="radio"
-                    id="cashless"
-                    value="cashless"
-                    {...register("paymentMethod", {
-                      required: "Please select a payment method",
-                    })}
-                    onChange={() => handlePaymentSelection("cashless")}
-                    checked={selectedPayment === "cashless"}
-                  />
+                  <div>
+                    <h3>CASHLESS</h3>
+                    <div
+                      className={`payment-option ${
+                        selectedPayment === "cashless" ? "selected" : ""
+                      }`}
+                      onClick={() => handlePaymentSelection("cashless")}
+                    >
+                      <img src={googlePay} alt="Cashless Payment" />
+                      <input
+                        type="radio"
+                        id="cashless"
+                        value="cashless"
+                        {...register("paymentMethod", {
+                          required: "Please select a payment method",
+                        })}
+                        onChange={() => handlePaymentSelection("cashless")}
+                        checked={selectedPayment === "cashless"}
+                      />
+                    </div>
+                  </div>
                 </div>
+
+                {errors.paymentMethod && (
+                  <p className="errorText">{errors.paymentMethod.message}</p>
+                )}
               </div>
-            </div>
 
-            {errors.paymentMethod && (
-              <p className="errorText">{errors.paymentMethod.message}</p>
-            )}
+              <input type="submit" />
+            </form>
           </div>
-
-          <input type="submit" />
-        </form>
-      </>
+        </>
+      )}
     </div>
   );
 };
