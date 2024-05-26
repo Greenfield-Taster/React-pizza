7bf49bfabb2fd56d0b0438f85a29c12241edae02
Anastasiia Horbachova <gorbatchovaasa123@gmail.com>
2024-05-26 17:07:46 +0300

    Updates

--- src/utils/calcTotalPrice.js
+++
@@ -1,7 +0,0 @@
-import { CartItem } from "../redux/slices/cartSlice";
-
-export const calcTotalPrice = (items: CartItem[]) => {
-  return (state.totalPrice = state.items.reduce((sum, obj) => {
-    return obj.price * obj.count + sum;
-  }, 0));
-};
